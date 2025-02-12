8313d16ccfd0055a59b913cec0225322ee94ac11
Léa Garaix <lea.garaix@etu.univ-grenoble-alpes.fr>
2025-02-12 21:15:28 +0100

    Pierre-feuille-ciseaux fin (mode portrait).

--- AndroidTP/app/src/main/java/fr/iut2/androidtp/Exercice3Activity.java
+++ AndroidTP/app/src/main/java/fr/iut2/androidtp/Exercice3Activity.java
@@ -9,17 +9,17 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import fr.iut2.androidtp.exercice3Data.Jeu;
+import fr.iut2.androidtp.exercice3Data.Resultat;
 
 public class Exercice3Activity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView papierOrdinateur, caillouOrdinateur, ciseauxOrdinateur;
     private ImageView papierJoueur, caillouJoueur, ciseauxJoueur;
-    private TextView resultat;
+    private TextView gagnant;
     private TextView nombreVictoires, nombreDefaites, nombreEgalites;
 
-    private int victoires = 0;
-    private int defaites = 0;
-    private int egalites = 0;
+    // Objet Resultat pour conserver les scores de la partie
+    Resultat resultat = new Resultat();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +39,7 @@ public class Exercice3Activity extends AppCompatActivity implements View.OnClick
         ciseauxJoueur = findViewById(R.id.joueur_ciseaux);
 
         // Récupération des textes pour les scores et le résultat
-        resultat = findViewById(R.id.exercice3_resultat);
+        gagnant = findViewById(R.id.exercice3_resultat);
         nombreVictoires = findViewById(R.id.exercice3_nombre_victoires);
         nombreDefaites = findViewById(R.id.exercice3_nombre_defaites);
         nombreEgalites = findViewById(R.id.exercice3_nombre_egalites);
@@ -48,36 +48,83 @@ public class Exercice3Activity extends AppCompatActivity implements View.OnClick
         papierJoueur.setOnClickListener(this);
         caillouJoueur.setOnClickListener(this);
         ciseauxJoueur.setOnClickListener(this);
-
-        // On attend que le joueur joue
-        // Quand il clique sur une image, on lance un nouveau jeu
-        // On set la MainJoueur du nouveau jeu
-        // On appelle la méthode egalite() sur le jeu
-        // Puis la méthode joueurGagne()
-
     }
 
     @Override
     public void onClick(View view) {
 
+        // On lance un jeu, qui détermine la main de l'ordinateur
         Jeu jeu = new Jeu();
+        // On réinitialise la visibilité et le fond des images
+        papierOrdinateur.setVisibility(View.INVISIBLE);
+        caillouOrdinateur.setVisibility(View.INVISIBLE);
+        ciseauxOrdinateur.setVisibility(View.INVISIBLE);
+        papierJoueur.setBackgroundColor(getResources().getColor(R.color.black, getTheme()));
+        caillouJoueur.setBackgroundColor(getResources().getColor(R.color.black, getTheme()));
+        ciseauxJoueur.setBackgroundColor(getResources().getColor(R.color.black, getTheme()));
 
-        switch (view.getId()) {
+        // Afficher la main choisir par l'ordinateur
+        switch (jeu.getMainOrdinateur()) {
 
-            case R.id.joueur_papier:
-                jeu.setMainJoueur(Jeu.PAPIER);
-
+            case Jeu.PAPIER:
+                papierOrdinateur.setVisibility(View.VISIBLE);
                 break;
 
-            case R.id.joueur_caillou:
-                jeu.setMainJoueur(Jeu.CAILLOUX);
+            case Jeu.CAILLOUX:
+                caillouOrdinateur.setVisibility(View.VISIBLE);
                 break;
 
-            case R.id.joueur_ciseaux:
-                jeu.setMainJoueur(Jeu.CISEAUX);
+            case Jeu.CISEAUX:
+                ciseauxOrdinateur.setVisibility(View.VISIBLE);
                 break;
-
         }
+
+        // Associe la vue a une constante
+        int idMain;
+        if (view.getId() == R.id.joueur_papier) {
+            idMain = Jeu.PAPIER;
+        } else if (view.getId() == R.id.joueur_caillou) {
+            idMain = Jeu.CAILLOUX;
+        } else {
+            idMain = Jeu.CISEAUX;
+        }
+
+        // Afficher la main du joueur
+        switch (idMain) {
+            // - Enregistrer le choix du joueur dans l'objet Jeu
+            // - Encadrer la main choisie en couleur
+
+            case Jeu.PAPIER:
+                jeu.setMainJoueur(Jeu.PAPIER);
+                papierJoueur.setBackgroundColor(getResources().getColor(R.color.green, getTheme()));
+                break;
+
+            case Jeu.CAILLOUX:
+                jeu.setMainJoueur(Jeu.CAILLOUX);
+                caillouJoueur.setBackgroundColor(getResources().getColor(R.color.green, getTheme()));
+                break;
+
+            case Jeu.CISEAUX:
+                jeu.setMainJoueur(Jeu.CISEAUX);
+                ciseauxJoueur.setBackgroundColor(getResources().getColor(R.color.green, getTheme()));
+                break;
+        }
+
+        // Mettre à jour les scores et indiquer le résultat :
+        if (jeu.egalite()) {
+            resultat.addEgalite();
+            nombreEgalites.setText(String.valueOf(resultat.getNombreEgalite()));
+            gagnant.setText(getResources().getText(R.string.exercice3_egalite));
+        } else if (jeu.joueurGagne()) {
+            resultat.addVictoire();
+            nombreVictoires.setText(String.valueOf(resultat.getNombreVictoire()));
+            gagnant.setText(getResources().getText(R.string.exercice3_victoire));
+        } else {
+            resultat.addDefaite();
+            nombreDefaites.setText(String.valueOf(resultat.getNombreDefaite()));
+            gagnant.setText(getResources().getText(R.string.exercice3_defaite));
+        }
+
     }
 
 }
