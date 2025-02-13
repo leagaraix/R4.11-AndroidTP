1cbaaf3e4d7f149df486b99ea5557ba46f1629fa
Léa Garaix <lea.garaix@etu.univ-grenoble-alpes.fr>
2025-02-13 17:36:20 +0100

    TP2 Exo 4

--- AndroidTP/app/src/main/java/fr/iut2/androidtp/Exercice4Activity.java
+++ AndroidTP/app/src/main/java/fr/iut2/androidtp/Exercice4Activity.java
@@ -1,5 +1,6 @@
 package fr.iut2.androidtp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,23 +8,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Exercice4Activity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button valider;
+    private TextView instructions;
     private EditText entreePrenom;
+    private Button valider;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;     // Attribut dédié à l'attente d'un résultat après le lancement d'une autre activité
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // On charge le XML pour créer l'arbre graphique
-        setContentView(R.layout.activity_exercice4);
+        setContentView(R.layout.activity_exercice4);         // On charge le XML pour créer l'arbre graphique
 
         // Récupération des vues
-        valider = findViewById(R.id.exercice4_valider);
+        instructions = findViewById(R.id.exercice4_instructions);
         entreePrenom = findViewById(R.id.exercice4_entree_prenom);
+        valider = findViewById(R.id.exercice4_valider);
+
+        // Initialisation de l'ActivityResultLauncher
+         activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            instructions.setText(getResources().getString(R.string.exercice4_nouvelles_instructions));
+                        }
+                    }
+                }
+        );
 
         // Ecoute des événements
         valider.setOnClickListener(this);
@@ -39,7 +60,7 @@ public class Exercice4Activity extends AppCompatActivity implements View.OnClick
         helloActivity.putExtra(HelloActivity.PRENOM, entreePrenom.getText().toString());
 
         // Lancement de la demande de changement d'activité
-        startActivity(helloActivity);
+        activityResultLauncher.launch(helloActivity);
     }
 
 }
--- AndroidTP/app/src/main/java/fr/iut2/androidtp/HelloActivity.java
+++ AndroidTP/app/src/main/java/fr/iut2/androidtp/HelloActivity.java
@@ -1,6 +1,8 @@
 package fr.iut2.androidtp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,9 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HelloActivity extends AppCompatActivity {
+public class HelloActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String PRENOM = "";
+    // Définition d'une clé
+    public static final String PRENOM = "PRENOM";
 
     private Button changerPrenom, changerExercice;
 
@@ -33,9 +36,34 @@ public class HelloActivity extends AppCompatActivity {
         TextView helloPrenom = findViewById(R.id.exercice4_hello_texte);
         helloPrenom.setText(getResources().getString(R.string.exercice4_hello_texte, prenom));
 
-        // J'EN SUIS ICIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-        // IIIIIIIIIIIIIICCCCIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+        // Récupération des vues
+        changerPrenom = findViewById(R.id.exercice4_changer_prenom);
+        changerExercice = findViewById(R.id.exercice4_changer_exercice);
 
+        // Ecoute des événements
+        //changerPrenom.setOnClickListener(view -> finish());
+        changerPrenom.setOnClickListener(this);
+        changerExercice.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.exercice4_changer_prenom:
+                Intent retourChangementPrenom = new Intent();
+                setResult(RESULT_OK, retourChangementPrenom);
+                finish();
+                break;
+
+            case R.id.exercice4_changer_exercice:
+                // Demande d'intention pour l'activité MainActivity
+                Intent intent = new Intent(HelloActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                break;
+        }
     }
 }
